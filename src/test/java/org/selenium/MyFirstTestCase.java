be40b0c1dace710c713f1ebe774e01c04261e8ca
belcar-ceci <carbel.cecilia@gmail.com>
2023-04-19 18:34:23 +0200

    add files and structure with java and selenium

---
+++ src/test/java/org/selenium/MyFirstTestCase.java
@@ -0,0 +1,45 @@
+package org.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MyFirstTestCase {
+
+    @Test
+    public void guestCheckoutUsingDirectBankTransfer() throws InterruptedException {
+        // System.setProperty("webdriver.chrome.driver", "ceciliacarbajal\\Documentos\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.askomdch.com/");
+        driver.manage().window().maximize();
+        driver.findElement(By.cssSelector("#menu-item-1227 > a")).click();
+        driver.findElement(By.id("woocommerce-product-search-field-0")).sendKeys("Blue");
+        driver.findElement(By.cssSelector("button[value='Search']")).click();
+        Assert.assertEquals(
+                driver.findElement(By.cssSelector(".woocommerce-products-header__title.page-title")).getText(),
+                "Search results: “Blue”"
+        );
+        driver.findElement(By.cssSelector("a[aria-label='Add “Blue Shoes” to your cart']")).click();
+        Thread.sleep(5000);
+        driver.findElement(By.cssSelector("a[title='View cart']")).click();
+        Assert.assertEquals(
+                driver.findElement(By.cssSelector("td[class='product-name'] a")).getText(),
+                "Blue Shoes"
+        );
+        driver.findElement(By.cssSelector(".checkout-button")).click();
+        driver.findElement(By.id("billing_first_name")).sendKeys("demo");
+        driver.findElement(By.id("billing_last_name")).sendKeys("user");
+        driver.findElement(By.id("billing_address_1")).sendKeys("San Francisco");
+        driver.findElement(By.id("billing_city")).sendKeys("San Francisco");
+        driver.findElement(By.id("billing_postcode")).sendKeys("94188");
+        driver.findElement(By.id("billing_email")).sendKeys("askomdch@gmail.com");
+        driver.findElement(By.id("place_order")).click();
+        Thread.sleep(5000);
+        Assert.assertEquals(
+                driver.findElement(By.cssSelector(".woocommerce-notice")).getText(),
+                "Thank you. Your order has been received."
+        );
+    }
+}
